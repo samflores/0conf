aeda80b6c979a1351c35c913ff2785905a20c431
Samuel Flores <me@samflor.es>
2015-05-05 21:42:19 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /usr/local/bin/zsh
+
+origin=`pwd`
+
+[[ ! -a $origin/vim ]] && mkdir $origin/vim
+ln -s $origin/vim ~/.vim
+ln -s $origin/vim ~/.nvim
+ln -s $origin/vimrc ~/.vimrc
+ln -s $origin/vimrc ~/.nvimrc
+
+ln -s $origin/gitconfig ~/.gitconfig
+ln -s $origin/gitignore_global ~/.gitignore_global
+
+ln -s $origin/zsh ~/.zsh
+ln -s $origin/zshrc ~/.zshrc
+
+git submodule init
+git submodule update
+
+source ~/.zshrc
+zsh_recompile
