80c7ec6990860f463ee590068a89dd5edd548faf
Samuel Flores <me@samflor.es>
2022-07-01 14:59:37 -0300

    Use Lightspeed

--- nvim/lua/plugins/motionsearch.lua
+++ nvim/lua/plugins/motionsearch.lua
@@ -1,23 +1,9 @@
-local package = 'rlane/pounce.nvim'
-
-local config = function()
-  require('pounce').setup({
-    accept_keys = "JFKDLSAHGNUVRBYTMICEOXWPQZ",
-    debug = false,
-  })
-  local map = vim.api.nvim_set_keymap
-  local opt = { silent = true, noremap = true }
-
-  map('n', '<leader>/', "<cmd>Pounce<CR>", opt)
-end
+local package = 'ggandor/lightspeed.nvim'
 
 local M = {}
 
 function M.init(use)
-  use {
-    package,
-    config = config
-  }
+  use { package }
 end
 
 return M
