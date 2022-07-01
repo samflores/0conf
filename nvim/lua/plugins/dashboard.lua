2c079d36b8fe1af33514469bbc1049962e737728
Samuel Flores <me@samflor.es>
2022-07-01 14:54:22 -0300

    Use Telescope as fuzzy finder

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -5,15 +5,35 @@ local config = function()
   local alpha = require'alpha'
   local dashboard = require'alpha.themes.dashboard'
   dashboard.section.header.val = {
-    [[                        _____ __                               ]],
-    [[  __________    _____ _/ ____\  |   ____ _______  ____   ______]],
-    [[ /  ___/__  \  /     \\   __\|  |  / __ \\_  __ \/ __ \ /  ___/]],
-    [[ \___ \ / __ \_  | |  \|  |  |  |__  \_\ )|  | \/  ___/_\___ \ ]],
-    [[/______\______/__|_|__/|__|  |____/\____/ |__|   \_____/______\]],
+    [[              _-o#&&*''''?d:>b\_]],
+    [[          _o/"`''  '',, dMF9MMMMMHo_]],
+    [[       .o&#'        `"MbHMMMMMMMMMMMHo.]],
+    [[     .o"" '         vodM*$&&HMMMMMMMMMM?.]],
+    [[    ,'              $M&ood,~'`(&##MMMMMMH\]],
+    [[   /               ,MMMMMMM#b?#bobMMMMHMMML]],
+    [[  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk]],
+    [[ ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|`*L]],
+    [[|               |MMMMMMMMMMMMMMMMMMMMbMH'   T,]],
+    [[$H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?]],
+    [[]MMH#             ""*""""*#MMMMMMMMMMMMM'    -]],
+    [[MMMMMb_                   |MMMMMMMMMMMP'     :]],
+    [[HMMMMMMMHo                 `MMMMMMMMMT       .]],
+    [[?MMMMMMMMP                  9MMMMMMMM}       -]],
+    [[-?MMMMMMM                  |MMMMMMMMM?,d-    ']],
+    [[ :|MMMMMM-                 `MMMMMMMT .M|.   :]],
+    [[  .9MMM[                    &MMMMM*' `'    .]],
+    [[   :9MMk                    `MMM#"        -]],
+    [[     &M}                     `          .-]],
+    [[      `&.                             .]],
+    [[        `~,   .                     ./]],
+    [[            . _                  .-]],
+    [[              '`--._,dd###pp=""']],
   }
   dashboard.section.buttons.val = {
     dashboard.button("e", "  New file",              ":ene <BAR> startinsert <CR>"),
-    dashboard.button("h", "  Recently opened files", ",oh"),
+    dashboard.button("f", "  Find file",             ":Telescope find_files<CR>"),
+    dashboard.button("h", "  Recently opened files", ":Telescope oldfiles cwd_only=true<CR>"),
+    dashboard.button("b", "  Browse project",        ":Telescope file_browser grouped=true<CR>"),
     dashboard.button("q", "  Quit Neovim",           ":qa <CR>"),
   }
   local handle = io.popen('fortune')
--- nvim/lua/plugins/fuzzy.lua
+++ nvim/lua/plugins/fuzzy.lua
@@ -1,113 +1,69 @@
-local package = 'camspiers/snap'
-local rocks = { 'fzy' }
+local package = 'nvim-telescope/telescope.nvim'
+local dependencies = {
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope-dap.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  'BurntSushi/ripgrep',
+  'nvim-telescope/telescope-fzy-native.nvim',
+  'nvim-telescope/telescope-ui-select.nvim',
+}
 
 local config = function()
-  local has_snap = pcall(require, "snap")
-
-  if not has_snap then
-    return
-  end
-
-  local snap = require"snap"
-  local config = snap.config
-  local M = {}
-
-  local fd_args = { '-t', 'f' }
-
-  local custom_mappings = {
-    ["view-toggle-hide"] = {"?"}
-  }
-
-  local defaults = {
-    prompt = " >",
-    suffix = "",
-    consumer = "fzy",
-    mappings = custom_mappings,
-    preview_min_width = 80
-  }
-
-  local function file(opts)
-    local options = vim.tbl_extend("force", defaults, opts or {})
-
-    return config.file:with(options)
-  end
-
-  local vimgrep =
-    config.vimgrep:with(
-      vim.tbl_extend(
-        "force",
-        defaults,
-        {
-          prompt = " /",
-          -- filter_with = "cword",
-          limit = 50000
-        }
-      )
-    )
-
-  function M.setup()
-    snap.maps {
-      { "<leader>oo", file() {args = fd_args, try = { "fd.file" } }, {command = "files"} },
-      { "<leader>og", file() {producer = "git.file"}, {command = "git.files"} },
-      { "<leader>ob", file() {producer = "vim.buffer"}, {command = "buffers"} },
-      { "<leader>gg", vimgrep {}, {command = "grep"} },
-      { "<leader>oh", file() {producer = "vim.oldfile"}, {command = "oldfiles"} },
-      { "<leader>oH", file() {combine = {"vim.buffer", "vim.oldfile"}} }
+  local actions = require("telescope.actions")
+  -- local trouble = require("trouble.providers.telescope")
+  local action_layout = require("telescope.actions.layout")
+  require("telescope").setup {
+    defaults = {
+      mappings = {
+        n = {
+          ["<M-p>"] = action_layout.toggle_preview,
+          -- ["<c-t>"] = trouble.open_with_trouble,
+        },
+        i = {
+          ["<M-p>"] = action_layout.toggle_preview,
+          ["<esc>"] = actions.close,
+          -- ["<c-t>"] = trouble.open_with_trouble,
+        },
+      },
+    },
+    extensions = {
+      file_browser = {
+        layout_config = { preview_width = 0.7 },
+      },
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown {}
+      }
     }
+  }
+  -- require('telescope').load_extension('dap')
+  require('telescope').load_extension('file_browser')
+  require('telescope').load_extension('fzy_native')
+  require("telescope").load_extension("ui-select")
 
-    snap.register.map(
-      {"n"},
-      {"<leader>h"},
-      function()
-        snap.run {
-          reverse = true,
-          prompt = "?",
-          producer = snap.get "consumer.fzy"(snap.get "producer.vim.help"),
-          select = snap.get "select.help".select,
-          views = {snap.get "preview.help"},
-          mappings = custom_mappings
-        }
-      end
-    )
+  local map = vim.api.nvim_set_keymap
+  local opt = { noremap = true }
 
-    snap.register.map(
-      {"n"},
-      {"<leader>o."},
-      function()
-        snap.run(vim.tbl_extend("force", defaults, {
-          producer = snap.get'consumer.fzy'(
-            snap.get'producer.ripgrep.file'.args(
-              {},
-              vim.fn.fnamemodify(vim.fn.bufname(), ":p:.:h")
-            )
-          ),
-          select = snap.get'select.file'.select,
-          multiselect = snap.get'select.file'.multiselect,
-          views = {snap.get'preview.file'}
-        }))
-      end
-    )
+  map('n', '<leader>oo', '<cmd>Telescope find_files<CR>', opt)
+  map('n', '<leader>o.', '<cmd>Telescope find_files cwd=%:p:h<CR>', opt)
+  map('n', '<leader>ov', '<cmd>Telescope find_files cwd=~/.config/nvim/<CR>', opt)
+  map('n', '<leader>og', '<cmd>Telescope git_files<CR>', opt)
+  map('n', '<leader>ob', '<cmd>Telescope buffers<CR>', opt)
+  map('n', '<leader>oh', '<cmd>Telescope oldfiles cwd_only=true<CR>', opt)
+  map('n', '<leader>oH', '<cmd>Telescope oldfiles<CR>', opt)
 
-    snap.register.map(
-      {"n"},
-      {"<leader>ov"},
-      function()
-        snap.run(vim.tbl_extend("force", defaults, {
-          producer = snap.get'consumer.fzy'(
-            snap.get'producer.ripgrep.file'.args(
-              { cwd = "/home/samflores/.config/nvim" },
-              "/home/samflores/.config/nvim"
-            )
-          ),
-          select = snap.get'select.file'.select,
-          multiselect = snap.get'select.file'.multiselect,
-          views = {snap.get'preview.file'}
-        }))
-      end
-    )
-  end
+  map('n', '<leader>bb', '<cmd>Telescope file_browser grouped=true<CR>', opt)
+  map('n', '<leader>b.', '<cmd>Telescope file_browser path=%:p:h grouped=true<CR>', opt)
 
-  M.setup()
+  map('n', '<leader>ss', '<cmd>Telescope live_grep<CR>', opt)
+  map('n', '<leader>s.', '<cmd>Telescope grep_string<CR>', opt)
+
+  map('n', '<leader>lr', '<cmd>Telescope lsp_references<CR>', opt)
+  map('n', '<leader>la', '<cmd>Telescope lsp_code_actions<CR>', opt)
+
+  map('n', '<leader>gs', '<cmd>Telescope git_status<CR>', opt)
+  map('n', '<leader>gb', '<cmd>Telescope git_branches<CR>', opt)
+
+  map('n', '<leader>tS', '<cmd>Telescope treesitter<CR>', opt)
 end
 
 local M = {}
@@ -115,7 +71,7 @@ local M = {}
 function M.init(use)
   use {
     package,
-    rocks = rocks,
+    requires = dependencies,
     config = config
   }
 end
