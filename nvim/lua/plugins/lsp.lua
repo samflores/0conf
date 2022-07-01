20d921fb7059e7b99a9a188a9ca443cccca9bc7c
Samuel Flores <me@samflor.es>
2022-07-01 14:57:11 -0300

    Organize LSP settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,135 +1,127 @@
-local packagename = 'glepnir/lspsaga.nvim'
+local packagename = 'neovim/nvim-lspconfig'
 local dependencies = {
-  'neovim/nvim-lspconfig',
-  'nvim-lua/lsp-status.nvim',
+  'glepnir/lspsaga.nvim',
   'williamboman/nvim-lsp-installer',
   'simrat39/rust-tools.nvim',
+  'folke/trouble.nvim'
 }
 
 local config = function()
-  local lsp_status = require('lsp-status')
-  lsp_status.register_progress()
+  require("nvim-lsp-installer").setup({ automatic_installation = true })
+  require("lspsaga").init_lsp_saga({
+    border_style = "bold"
+  })
+  require("rust-tools").setup {}
+  require("trouble").setup {}
 
   local on_attach = function(client, bufnr)
-    local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function opt(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    lsp_status.on_attach(client)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
-    local opts = { noremap=true, silent=true }
-    map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    -- map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    map('n', 'gd', "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>", opts)
-    map('n', 'gh', "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", opts)
-    map('n', 'gs', "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>", opts)
-    -- map('n', 'M', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    -- map('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    -- map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    map('n', '<space>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
-    map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    --[[ map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts) ]]
-    map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    -- map('n', '<space>ac', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    map('n', '<space>ca', "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", opts)
-    map('v', '<space>ca', "<cmd>'<,'>lua require('lspsaga.codeaction').range_code_action()<CR>", opts)
-    map('n', 'M', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-    map('n', '<C-f>', "<cmd>lua require('lspsaga.hover').smart_scroll_hover(1)<CR>", opts)
-    map('n', '<C-b>', "<cmd>lua require('lspsaga.hover').smart_scroll_hover(-1)<CR>", opts)
+    local map = vim.keymap.set
+    local opts = { noremap = true, silent = true, buffer = bufnr }
 
-    map('n', '<space>dd', "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", opts)
-    map('n', '[d', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", opts)
-    map('n', ']d', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", opts)
+    map('n', 'gD', vim.lsp.buf.declaration, opts)
+    -- map('n', 'gd', vim.lsp.buf.definition, opts)
+    -- map('n', 'M', vim.lsp.buf.hover, opts)
+    map('n', 'gi', vim.lsp.buf.implementation, opts)
+    -- map('n', '<C-s>', vim.lsp.buf.signature_help, opts)
+    map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    map('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    map('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    -- map('n', '<space>rn', vim.lsp.buf.rename, opts)
+    map('n', 'gr', vim.lsp.buf.references, opts)
+    -- map('n', '<space>e', vim.lsp.diagnostic.show_line_diagnostics, opts)
+    -- map('n', '[d', vim.lsp.diagnostic.goto_prev, opts)
+    -- map('n', ']d', vim.lsp.diagnostic.goto_next, opts)
+    map('n', '<space>q', vim.lsp.diagnostic.set_loclist, opts)
+    -- map('n', '<space>ca', vim.lsp.buf.code_action, opts)
 
-    -- Set some keybinds conditional on server capabilities
+    map('n', 'gd', require 'lspsaga.definition'.preview_definition, opts)
+    map('n', 'gh', require 'lspsaga.finder'.lsp_finder, opts)
+    map('n', 'gs', require('lspsaga.signaturehelp').signature_help, opts)
+    map('n', '<space>rn', require('lspsaga.rename').lsp_rename, opts)
+    map('n', '<space>ca', require('lspsaga.codeaction').code_action, opts)
+    map('v', '<space>ca', function()
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
+      require('lspsaga.codeaction').range_code_action()
+    end, opts)
+    map('n', 'M', require('lspsaga.hover').render_hover_doc, opts)
+    local action = require("lspsaga.action")
+    map('n', '<C-f>', function() action.smart_scroll_with_saga(1) end, opts)
+    map('n', '<C-b>', function() action.smart_scroll_with_saga(-1) end, opts)
+    map('n', '<space>dd', require 'lspsaga.diagnostic'.show_line_diagnostics, opts)
+    map('n', '[d', require 'lspsaga.diagnostic'.goto_prev, opts)
+    map('n', ']d', require 'lspsaga.diagnostic'.goto_next, opts)
+
     if client.resolved_capabilities.document_formatting then
-      map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+      map("n", "<space>f", vim.lsp.buf.formatting, opts)
       vim.api.nvim_exec([[
-      augroup lsp_document_formatting
-      autocmd! * <buffer>
-      autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-      augroup END
-        ]], false)
-    elseif client.resolved_capabilities.document_range_formatting then
-      map("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        augroup lsp_document_formatting
+        autocmd! * <buffer>
+        autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
+        augroup END
+      ]], false)
+    end
+
+    if client.resolved_capabilities.document_range_formatting then
+      map("n", "<space>f", vim.lsp.buf.range_formatting, opts)
     end
 
     -- Set autocommands conditional on server_capabilities
     if client.resolved_capabilities.document_highlight then
       vim.api.nvim_exec([[
-      augroup lsp_document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-        ]], false)
+        augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+      ]], false)
     end
   end
 
-  -- Configure lua language server for neovim development
-  local lua_settings = {
-    Lua = {
-      runtime = {
-        -- LuaJIT in the case of Neovim
-        version = 'LuaJIT',
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+  local lsp_config = require('lspconfig')
+  lsp_config['clangd'].setup { on_attach = on_attach }
+  lsp_config['clojure_lsp'].setup { on_attach = on_attach }
+  lsp_config['cssls'].setup { on_attach = on_attach }
+  lsp_config['dartls'].setup { on_attach = on_attach }
+  lsp_config['denols'].setup {
+    on_attach = on_attach,
+    root_dir = lsp_config.util.root_pattern("deno.json", "deno.jsonc"),
+  }
+  -- lsp_config['ember'].setup { on_attach = on_attach }
+  lsp_config['emmet_ls'].setup { on_attach = on_attach }
+  lsp_config['graphql'].setup { on_attach = on_attach }
+  lsp_config['html'].setup { on_attach = on_attach }
+  lsp_config['jsonls'].setup { on_attach = on_attach }
+  lsp_config['solargraph'].setup { on_attach = on_attach }
+  lsp_config['sqlls'].setup { on_attach = on_attach }
+  lsp_config['sumneko_lua'].setup {
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = vim.split(package.path, ';'),
         },
-      },
+        diagnostics = {
+          globals = { 'vim' },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+      }
     }
   }
-
-  -- config that activates keymaps and enables snippet support
-  local function make_config()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    return {
-      -- enable snippet support
-      capabilities = capabilities,
-      -- map buffer local keybindings when the language server attaches
-      on_attach = on_attach,
-    }
-  end
-
-  local lsp_installer = require("nvim-lsp-installer")
-
-  lsp_installer.on_server_ready(function(server)
-    local opts = make_config()
-
-    if server.name == "sumneko_lua" then
-      opts.config = lua_settings
-    elseif server.name == "rust_analyzer" then
-      require("rust-tools").setup {
-        server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
-      }
-      -- server:attach_buffers()
-      return
-    end
-
-    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-  end)
-
-  -- require("diaglist").init()
+  lsp_config['svelte'].setup { on_attach = on_attach }
+  lsp_config['tsserver'].setup {
+    on_attach = on_attach,
+    root_dir = lsp_config.util.root_pattern("package.json"),
+  }
 end
 
 local M = {}
