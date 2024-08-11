575196d05949e51fb680fd560aadfa332891c0b6
Samuel Flores <me@samflor.es>
2024-08-10 22:31:09 -0300

    Update REST client to use LuaRocks

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -1,12 +1,21 @@
 return {
-  'rest-nvim/rest.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+    }
   },
-  keys = {
-    { '<leader>hh', '<Plug>RestNvim' },
-    { '<leader>hu', '<Plug>RestNvimPreview' },
-    { '<leader>hl', '<Plug>RestNvimLast' },
-  },
-  config = true
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = true,
+    keys = {
+      { '<leader>hh', '<Plug>RestNvim' },
+      { '<leader>hu', '<Plug>RestNvimPreview' },
+      { '<leader>hl', '<Plug>RestNvimLast' },
+    },
+  }
 }
