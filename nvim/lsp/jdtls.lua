27fa2b6b00a32bedd74103737568d08349c244ec
Samuel Flores <me@samflor.es>
2026-04-18 09:51:29 -0300

    nvim: use stdpath('cache') for jdtls config and workspace

--- nvim/lsp/jdtls.lua
+++ nvim/lsp/jdtls.lua
@@ -1,5 +1,5 @@
 return {
-  cmd = { 'jdtls', '-configuration', '/home/runner/.cache/jdtls/config', '-data', '/home/runner/.cache/jdtls/workspace' },
+  cmd = { 'jdtls', '-configuration', vim.fn.stdpath('cache') .. '/jdtls/config', '-data', vim.fn.stdpath('cache') .. '/jdtls/workspace' },
   filetypes = { 'java' },
   root_markers = { '.git', 'build.gradle', 'build.gradle.kts', 'build.xml', 'pom.xml', 'settings.gradle', 'settings.gradle.kts' },
 }
