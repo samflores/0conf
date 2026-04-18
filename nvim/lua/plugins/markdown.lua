e2e89e464d690161cc1be73f5d911473625c4b34
Samuel Flores <me@samflor.es>
2026-04-18 09:53:09 -0300

    nvim: add markview.nvim for markdown rendering

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -6,4 +6,24 @@ return {
       cmd = 'LivePreview',
     },
   },
+  {
+    src = 'https://github.com/OXY2DEV/markview.nvim.git',
+    name = 'markview',
+    data = {
+      ft = { 'markdown' }
+    }
+  }
+  -- {
+  --   src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  --   name = 'render-markdown.nvim',
+  --   data = {
+  --     dep_of = 'avante.nvim',
+  --     ft = { 'markdown', 'Avante' },
+  --     after = function()
+  --       require('render-markdown').setup({
+  --         file_types = { 'markdown', 'Avante' },
+  --       })
+  --     end,
+  --   },
+  -- },
 }
