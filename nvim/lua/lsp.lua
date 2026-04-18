0f4b7f88a697959c376659537fd5c434e931db11
Samuel Flores <me@samflor.es>
2026-04-18 09:49:37 -0300

    nvim: rewrite hover/signatureHelp handlers as wrappers
    vim.lsp.with is gone in current nvim; wrap the default handlers
    directly to inject rounded borders.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -27,10 +27,14 @@ vim.lsp.enable({
   'yamlls',
 })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'rounded', -- or "single", "double", "shadow", etc.
-})
+vim.lsp.handlers['textDocument/hover'] = function(err, result, ctx, config)
+  config = config or {}
+  config.border = 'rounded'
+  return vim.lsp.handlers.hover(err, result, ctx, config)
+end
 
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'rounded',
-})
+vim.lsp.handlers['textDocument/signatureHelp'] = function(err, result, ctx, config)
+  config = config or {}
+  config.border = 'rounded'
+  return vim.lsp.handlers.signature_help(err, result, ctx, config)
+end
