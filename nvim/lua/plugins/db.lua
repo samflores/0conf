c087af7d8ea8a5571cfa6a63d73df840800fa853
Samuel Flores <me@samflor.es>
2026-06-17 17:35:15 -0300

    nvim: replace nvim-dbee with dadbod-grip.nvim
    Switch the database plugin to joryeugene/dadbod-grip.nvim, a pure-Lua
    client (no Node/telescope/vim-dadbod deps) that shells out to
    psql/sqlite3/mysql/duckdb.
    
    - <leader>D keymaps: connect, schema, tables, query pad, grid, history
    - Run query on <S-CR>; run and open results in a new split on <C-S-CR>
    - Tint the selected-column highlight (GripColHighlight) with the
      colorscheme's Comment bg, re-applying on ColorScheme and grip buffer
      enter since the plugin resets it inside M.open()
    - Auto-close the welcome screen on connect so the query pad fills the
      right column; query results then open as a split below it

--- nvim/lua/plugins/db.lua
+++ nvim/lua/plugins/db.lua
@@ -1,26 +1,48 @@
 return {
   {
-    src = 'https://github.com/MunifTanjim/nui.nvim',
-    name = 'nui.nvim'
-  },
-  {
-    src = 'https://github.com/kndndrj/nvim-dbee',
-    name = 'nvim-dbee',
+    src = 'https://github.com/joryeugene/dadbod-grip.nvim',
+    name = 'dadbod-grip.nvim',
     data = {
       cmd = {
-        'Dbee'
+        'Grip', 'GripConnect', 'GripTables', 'GripQuery',
+        'GripSchema', 'GripHistory', 'GripStart',
       },
       keys = {
-        { lhs = '<leader>D', rhs = function() require('dbee').toggle() end }
+        { lhs = '<leader>Dc', rhs = '<cmd>GripConnect<cr>', desc = 'DB: pick/manage connections' },
+        { lhs = '<leader>Ds', rhs = '<cmd>GripSchema<cr>',  desc = 'DB: browse schema (dbs/tables)' },
+        { lhs = '<leader>Dt', rhs = '<cmd>GripToggle<cr>',  desc = 'DB: toggle the Grip UI' },
+        { lhs = '<leader>DT', rhs = '<cmd>GripTables<cr>',  desc = 'DB: pick a table' },
+        { lhs = '<leader>Dq', rhs = '<cmd>GripQuery<cr>',   desc = 'DB: open query pad' },
+        { lhs = '<leader>Dg', rhs = '<cmd>Grip<cr>',        desc = 'DB: open editable grid' },
+        { lhs = '<leader>Dh', rhs = '<cmd>GripHistory<cr>', desc = 'DB: query history' },
       },
-      before = function()
-        vim.cmd.packadd('nui.nvim')
-      end,
       after = function()
-        require('dbee').setup({
-          drawer = {
-            disable_help = false
-          }
+        require('dadbod-grip').setup({
+          picker = 'snacks',
+          limit = 100,
+          max_col_width = 40,
+          timeout = 30000,
+          border = 'rounded',
+          keymaps = {
+            qpad_execute     = '<S-CR>',
+            qpad_execute_new = '<C-S-CR>',
+          },
+        })
+
+        local set_col_hl = function()
+          local c = vim.api.nvim_get_hl(0, { name = 'Comment', link = false })
+          local col = c.bg or c.fg
+          if col then vim.api.nvim_set_hl(0, 'GripColHighlight', { bg = col }) end
+        end
+        set_col_hl()
+        vim.api.nvim_create_augroup('DadbodGripColHl', { clear = true })
+        vim.api.nvim_create_autocmd('ColorScheme', {
+          group = 'DadbodGripColHl', callback = set_col_hl,
+        })
+        local grip_buf = function() return vim.api.nvim_buf_get_name(0):match('^grip://') end
+        vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinEnter' }, {
+          group = 'DadbodGripColHl',
+          callback = function(a) if grip_buf() then set_col_hl() end end,
         })
       end,
     },
